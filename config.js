e9ee55c5eae65de02bf27c53da8e1c31eec1e14b
excliver777 <75602159+excliver777@users.noreply.github.com>
2021-06-17 19:29:24 +0800

    adsf

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: "ODU0OTM3NDAxNjA3OTc5MDY4.YMrMgA.3xG2uzXQpE24iquiQD6zzG72f10",
+    channel: "853343890958712873",
+    messageTemplate: " **{author}** 님이 **{title}**를 업로드를 했어요 !!\n{url}",
+    channel_id: "UCXu13K308hUzQugidTDEK5Q",
+    watchInterval: 100
+}
